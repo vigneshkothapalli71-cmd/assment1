1fec3b33b666801ea7a31b05e1a1a0f4b6785ac3
vigneshkothapalli <vigneshkothapalli71@gmail.com>
2026-01-17 14:24:47 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,11 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+x=a+b
+y=a-b
+z=a*b
+u=a%b
+print("addition:",x)
+print("subtraction:",y)
+print("multiplication:",z)
+print("division:",u)
+
---
+++ task2.py
@@ -0,0 +1,3 @@
+a=input("enter your first name:")
+b=input("enter your second name:")
+print("Hello,",a,b+'!',"welcom to the python program")
